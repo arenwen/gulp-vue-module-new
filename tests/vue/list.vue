356e3423191907a773f982993ac1e9274ae818e7
arenwen <renwenqing@cicaero.com>
2018-01-11 16:19:48 +0800

    支持less

---
+++ tests/vue/list.vue
@@ -0,0 +1,32 @@
+<style lang="less">
+    body{
+        background: red;
+        div{
+            background: #fff;
+        }
+    }
+</style>
+<template>
+    <div class="list">
+        {{a}}'dfsadf'
+        <aaa :msg="a"></aaa>
+    </div>
+</template>
+
+<script>
+    var a   = require("a");
+    var Vue = require("vue");
+
+    module.exports = Vue.component("xxx", {
+        data : function() {
+            return {
+                a : 23456
+            }
+        },
+        template : '__template__',
+        props : ["msg"],
+        components: {
+            aaa : a
+        }
+    });
+</script>
